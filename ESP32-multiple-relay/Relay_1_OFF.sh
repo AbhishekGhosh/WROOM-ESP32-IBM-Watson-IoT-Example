59cb3039e8eec3f145e921f4aeb7f9bf99fe08de
Abhishek Ghosh <dr.abhishek_ghosh@hotmail.com>
2019-09-11 15:21:34 +0530

    Create Relay_1_OFF.sh

---
+++ ESP32-multiple-relay/Relay_1_OFF.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  
+
+CURL='/usr/bin/curl'
+
+curl -u <use-the-API-Key>:<use-auth-token> -H "Content-Type: text/plain" -v -X POST http://<your org>.messaging.internetofthings.ibmcloud.com:1883/api/v0002/application/types/<yourDeviceType>/devices/<yourDeviceId>/commands/pin1 -d "off"
+
+# or you can redirect it into a file:
+
+# $CURL  > watson.log
